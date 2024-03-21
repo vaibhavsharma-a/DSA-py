993cbfe49a8dd05269b79731aad7ed7af44e15fc
Vaibhav <vaibhav1863sharma@gmail.com>
2024-03-21 23:02:54 +0530

    More question on array

--- array_list.py
+++ array_list.py
@@ -62,7 +62,7 @@
 
 #! remove duplicate and return the number of unique elem
 
-arr = [1,1,2,2,3,4,6,6,7]
+# arr = [1,1,2,2,3,4,6,6,7]
 
 # num_set = set()
 
@@ -132,24 +132,24 @@ arr = [1,1,2,2,3,4,6,6,7]
 # arr.reverse()
 # print(arr)
 
-def reverse(arr,str,end):
-  while str<end:
-    arr[str],arr[end] = arr[end],arr[str]
-    str += 1
-    end -= 1
+# def reverse(arr,str,end):
+#   while str<end:
+#     arr[str],arr[end] = arr[end],arr[str]
+#     str += 1
+#     end -= 1
 
-def rotate_by_d(arr,n,d):
-  d = d%n
+# def rotate_by_d(arr,n,d):
+#   d = d%n
 
-  reverse(arr,0,d-1)
-  reverse(arr,d,n-1)
-  reverse(arr,0,n-1)
+#   reverse(arr,0,d-1)
+#   reverse(arr,d,n-1)
+#   reverse(arr,0,n-1)
 
 
-arr = [10,23,45,66,23,56,45,62]
-n = len(arr)
-rotate_by_d(arr,n,2)
-print(arr)
+# arr = [10,23,45,66,23,56,45,62]
+# n = len(arr)
+# rotate_by_d(arr,n,2)
+# print(arr)
 #! ecluidian algo
 
 # def cal_gcd(a,b):
@@ -161,3 +161,199 @@ print(arr)
 # b =int(input("Enter another number"))
 # print(f"The GCD is of {a} and {b} is {cal_gcd(a,b)}")
 
+#! move zeroes to the end
+#? brute
+
+# arr = [1,2,0,7,0,5,3,0,7,4,0,66,0,4,0,0,5]
+
+# temp = []
+
+# for i in range(0,len(arr)):
+#   if arr[i] != 0:
+#     temp.append(arr[i])
+
+# non_zero_value = len(temp)
+
+# for i in range(0,non_zero_value):
+#   arr[i] = temp[i]
+
+# for i in range(non_zero_value,len(arr)):
+#   arr[i] = 0
+
+# print(arr)
+
+#? optimal
+
+# j = -1
+# for i in range(0,len(arr)):
+#   if arr[i] == 0:
+#     j = i
+#     break
+
+# for i in range(j+1,len(arr)):
+#   if arr[i] != 0:
+#     arr[i],arr[j] = arr[j],arr[i]
+#     j += 1
+
+# print(arr)
+
+#! union in two sorted array 
+
+#? Brute force
+
+# arr1 = [1,1,2,4,5,6,3,4]
+# arr2 = [3,4,5,7,8,9,9,1,4]
+
+# uni = set()
+
+# for i in range(0,len(arr1)):
+#   uni.add(arr1[i])
+
+# for i in range(0,len(arr2)):
+#   uni.add(arr2[i])
+
+# union_arr = [num for num in uni]
+
+#? Optimal
+
+# def union_array(arr1,arr2):
+#   n1 = len(arr1)
+#   n2 = len(arr2)
+#   i = 0
+#   j = 0
+
+#   union_arr = []
+
+#   while(i< n1 and j<n2):
+#     if(arr1[i] <= arr2[j]):
+#       if(len(union_arr) == 0 or union_arr[-1]!=arr1[i]):
+#         union_arr.append(arr1[i])
+#       i+= 1
+
+#     else:
+#       if(len(union_arr) == 0 or union_arr[-1]!= arr2[j]):
+#         union_arr.append(arr2[j])
+#       j+=1
+
+#   while(j<n2):
+#     if(len(union_arr) == 0 or union_arr[-1]!= arr2[j]):
+#       union_arr.append(arr2[j])
+#     j+=1
+
+#   while(i<n1):
+#     if(len(union_arr) == 0 or union_arr[-1]!=arr1[i]):
+#       union_arr.append(arr1[i])
+#     i+= 1
+  
+#   return union_arr
+
+# arr1 = [1,1,2,4,5,6]
+# arr2 = [3,4,5,7,8,9,9]
+# print(union_array(arr1,arr2))
+# print(union_array)
+
+
+#! intersection of two sorted arrays
+
+#? brute
+arr1 = [1,1,2,3,4,5,6]
+arr2 = [1,2,3,3,4,4,6,7]
+
+n1 = len(arr1)
+n2 = len(arr2)
+
+# visted_array = [0]*len(arr2)
+
+# intersection_array = []
+
+# for i in range(0,n1):
+#   for j in range(0,n2):
+#     if(arr1[i] == arr2[j] and visted_array[j]==0):
+#       intersection_array.append(arr1[i])
+#       visted_array[j] = 1
+#       break
+
+#     if(arr2[j]>arr1[i]):
+#       break
+
+# print(intersection_array)
+
+#? optimal
+ 
+# i = 0 
+# j = 0
+# answer = []
+
+# while(i<n1 and j<n2):
+#   if (arr1[i] < arr2[j]):
+#     i += 1
+
+#   elif(arr1[i] > arr2[j]):
+#     j+= 1
+
+#   elif(arr1[i] == arr2[j]):
+#     answer.append(arr1[i])
+#     i+=1
+#     j+=1
+
+
+# print(answer)
+
+#! missing number in an arry 
+
+#? brute force
+
+arr = [1,2,3,4]
+n = 5
+
+# for i in range(1,n+1):
+#   flag = 0
+#   for j in range(0,n-1):
+#     if arr[j] == i:
+#       flag = 1
+#       break
+
+#   if flag == 0:
+#     print(i)
+
+#? better
+# hased_array = [0] * int(n+1)
+# # print(hased_array)
+# for i in range(0,n-1):
+#   hased_array[arr[i]] = 1
+
+# # print(hased_array)
+# for i in range(1,n+1):
+#   if hased_array[i] == 0:
+#     print(i)
+
+#?Optimal 
+
+#! using sum
+def missing_elemet(arr,n):
+  sum_0f_n = n*(n+1)//2
+  s1 = 0
+  for i in range(0,n-1):
+    s1 += arr[i]
+
+  difference = sum_0f_n - s1
+  return difference
+
+print(missing_elemet(arr,n))
+
+#! using xor
+
+def missing_num(arr,n):
+  xor1 = 0
+  for i in range(1,n+1):
+    xor1 = xor1 ^ i
+  
+  xor2 = 0
+  for i in range(0,n-1):
+    xor2 = xor2 ^ arr[i]
+
+
+
+  return xor1 ^ xor2
+
+print(missing_num(arr,n))
